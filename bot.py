7a70df6ba1d3fd0ada03c75905edff5befc7dc4c
Mohammad Nakhjiri <mohammaddev@outlook.com>
2023-04-14 20:48:36 +0330

    debugging

--- bot.py
+++ bot.py
@@ -22,8 +22,9 @@ def save_user_to_db(func):
         if message.chat.last_name is None:
             message.chat.last_name = ""
         User.add_user(message.chat.first_name, message.chat.last_name, message.chat.id)
+        func(message)
 
-    return func
+    return wrapper_func
 
 
 # base bot
