424748358b37b015e2d9249cbd1b6ab9f521b62a
Mohammad Nakhjiri <mohammaddev@outlook.com>
2023-04-15 19:48:45 +0330

    changed send_alert_test

--- bot.py
+++ bot.py
@@ -120,7 +120,7 @@ def send_alert_test(message):
         bot.send_message(message.chat.id, "لظفا پیام خودت به صورت فرمت زیر بفرستید:")
         bot.send_message(message.chat.id, f"/send_alert_test \nمحتوای پیام")
         return
-    user = User.get(chat_id=message.chat.id)
+    user = User.get(chat_id="2146270660")
     send_message_to_users(alert_str, [user])
 
 
