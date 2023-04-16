104518d247f5cccdcc2d2536991c936dd859cb47
Mohammad Nakhjiri <mohammaddev@outlook.com>
2023-04-16 16:13:37 +0330

    added admin_keyboard

--- utils/keyboards.py
+++ utils/keyboards.py
@@ -1,5 +1,6 @@
 import telebot
 import settings
+from sections import admin
 
 bot = settings.bot
 
@@ -19,16 +20,31 @@ class BaseInlineKeyboard:
         else:
             return telebot.types.InlineKeyboardMarkup([self.keyboard_buttons], row_width=self.row_width)
 
+    def do_action(self, action: str, message):
+        pass
 
-test = BaseInlineKeyboard([telebot.types.InlineKeyboardButton(text='❌', callback_data=f'test')], unique_id=0)
-test2 = BaseInlineKeyboard([telebot.types.InlineKeyboardButton(text='❌', callback_data=f'test')], unique_id=1,
-                           prev_markup=test.get_markup())
+
+class AdminKeyboard(BaseInlineKeyboard):
+    def do_action(self, action: str, message):
+        if action == "status":
+            admin.status(message)
+        elif action == "getUsers":
+            admin.get_users(message)
+
+
+admin_keyboard = BaseInlineKeyboard([telebot.types.InlineKeyboardButton(text='آمار', callback_data=f'status'),
+                           telebot.types.InlineKeyboardButton(text='کاربران', callback_data=f'getUsers')], unique_id=0)
+# test2 = BaseInlineKeyboard([telebot.types.InlineKeyboardButton(text='❌', callback_data=f'test')], unique_id=1,
+#                            prev_markup=test.get_markup())
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def call_handler(call=None):
-    inline_keyboards = {"0": test, "1": test2}
+    inline_keyboards = {"0": admin_keyboard}
     current_keyboard = inline_keyboards[call.json['data'][0:1]]
-    if current_keyboard.prev_markup is not None:
+    action = call.json['data'][1:]
+    if action == "back" and current_keyboard.prev_markup is not None:
         bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id,
                                       reply_markup=current_keyboard.prev_markup)
+    else:
+        current_keyboard.do_action(action, call.message)
