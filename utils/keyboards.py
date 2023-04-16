8eacbd4673ad282a77235f2c048504cc35ae5acd
Mohammad Nakhjiri <mohammaddev@outlook.com>
2023-04-16 16:01:29 +0330

    fixed a bug

--- utils/keyboards.py
+++ utils/keyboards.py
@@ -14,7 +14,7 @@ class BaseInlineKeyboard:
     def get_markup(self):
         if self.prev_markup is not None:
             keyboards = self.keyboard_buttons
-            keyboards.append([telebot.types.InlineKeyboardButton(text='❌', callback_data=f'{self.unique_id}back')])
+            keyboards.append(telebot.types.InlineKeyboardButton(text='❌', callback_data=f'{self.unique_id}back'))
             return telebot.types.InlineKeyboardMarkup([keyboards], row_width=self.row_width)
         else:
             return telebot.types.InlineKeyboardMarkup([self.keyboard_buttons], row_width=self.row_width)
@@ -30,4 +30,5 @@ def call_handler(call=None):
     inline_keyboards = {"0": test, "1": test2}
     current_keyboard = inline_keyboards[call.json['data'][0:1]]
     if current_keyboard.prev_markup is not None:
-        bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, current_keyboard.prev_markup)
+        bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id,
+                                      reply_markup=current_keyboard.prev_markup)
