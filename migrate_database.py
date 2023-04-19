ce76766ac1aaf43217b45abef32643e3ef3f9d94
Mohammad Nakhjiri <mohammaddev@outlook.com>
2023-04-20 02:02:20 +0330

    migrating data

--- migrate_database.py
+++ migrate_database.py
@@ -4,3 +4,5 @@ import database
 for user_record in database_temp.User.select():
     database.User.add_user(user_record.name, user_record.last_name, user_record.chat_id)
 
+for record in database_temp.BlackListWord.select():
+    database.BlackListWord.add_to_black_list(record.text, record.user.chat_id)
