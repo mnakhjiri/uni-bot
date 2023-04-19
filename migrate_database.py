389ebccaaea8bd8aea5ccfcd3f49dad02f186365
Mohammad Nakhjiri <mohammaddev@outlook.com>
2023-04-20 01:59:15 +0330

    migrating data

--- migrate_database.py
+++ migrate_database.py
@@ -1,5 +1,6 @@
 import database_temp
 import database
 
-for sqlite_record in database_temp.database.select():
-    database.database.create(name=sqlite_record.name, age=sqlite_record.age)
+for user_record in database_temp.User.select():
+    database.User.add_user(user_record.name, user_record.last_name, user_record.chat_id)
+
