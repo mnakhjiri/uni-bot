e0a8dfe88980aa640a7889dab803ff9c9edd9ba7
Mohammad Nakhjiri <mohammaddev@outlook.com>
2023-04-20 02:26:27 +0330

    testing migrations

--- database.py
+++ database.py
@@ -90,6 +90,7 @@ class Session(BaseModel):
     user = ForeignKeyField(User)
     waiting_action = TextField()
     json_saved_data = TextField(null=True)
+    test = TextField(null=True)
 
     @classmethod
     def create_session(cls, chat_id: str, waiting_action: str, json_saved_data=None):
