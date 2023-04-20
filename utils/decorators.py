0db6bab7fe1750925c5a11361066a1c72ccb1537
Mohammad Nakhjiri <mohammaddev@outlook.com>
2023-04-20 21:06:37 +0330

    fix a bug

--- utils/decorators.py
+++ utils/decorators.py
@@ -41,5 +41,6 @@ def save_action(action=None):
             BotLog.add_log(*args)
             # executor.submit(BotLog.add_log, args)
             func(message)
+            return wrapper
 
     return wrapper_function
