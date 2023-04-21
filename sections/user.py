0567c5abfd8f6c429cd6d3451fa06b86ae26da47
Mohammad Nakhjiri <mohammaddev@outlook.com>
2023-04-21 11:06:20 +0330

    fix a bug

--- sections/user.py
+++ sections/user.py
@@ -156,8 +156,7 @@ def send_foods(message):
         bot.send_message(message.chat.id, "شما نمی توانید بیشتر از یک کد فراموشی در روز بگیرید.")
         return
     foods = list(
-        FoodCode.select().execute())
-    print(datetime.utcnow() - timedelta(days=1))
+        FoodCode.select().where(FoodCode.time_created > (datetime.utcnow() - timedelta(days=1))).execute())
     if len(foods) == 0:
         bot.send_message(message.chat.id, "غذایی در لیست امروز باقی نمانده است.")
         return
